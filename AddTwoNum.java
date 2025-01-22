c2f679eefbb96b6d08e2f45f5ae6482346a6913d
Dipak Sah <dipaksah@Dipaks-MacBook-Air.local>
2025-01-23 01:13:17 +0530

    AddTwoNum

---
+++ AddTwoNum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class AddTwoNum {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter Number first : ");
+        int num1 = obj.nextInt();
+        System.out.println("Enter second Number : ");
+        int num2 = obj.nextInt();
+    }
+}
