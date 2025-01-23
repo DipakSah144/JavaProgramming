0ae92bbd8544acba81b5e3d5969208652764ad05
Dipak Sah <dipaksah@Dipaks-MacBook-Air.local>
2025-01-23 10:12:32 +0530

    code is update

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
+import java.util.*;
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hi EveryOne");
+        Scanner obj = new Scanner(System.in);
+        int r,sum = 0,temp;
+        System.out.println("Enter a number : ");
+        int num = obj.nextInt();
+        temp = n;
+        while(n>0){
+            r = n % 10;
+            sum=(sum*10)+r;
+            n=n/10;
+        }
+        if(temp == sum){
+            System.out.println("Number is palindrom number");
+        }else{
+            System.out.println("Number is not palindrome number");
+        }
+
     }
 }
